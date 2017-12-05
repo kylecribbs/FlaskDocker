fa97e786bdce9252598022fff583f0e554d7bcf2
kylecribbs <31107878+kylecribbs@users.noreply.github.com>
2017-12-04 20:34:48 -0700

    updated code

--- db/init_db.sh
+++ db/init_db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 /usr/bin/mysqld_safe --skip-grant-tables &
 sleep 5
-mysql -u root -e "CREATE DATABASE db625024825"
-mysql -u root db625024825 < /tmp/dump.sql
+mysql_upgrade -u dbo625024825 -pQi>^#$u3xEqR<dtIf~mU
+mysql -u dbo625024825 -pQi>^#$u3xEqR<dtIf~mU db625024825 < /tmp/dump.sql
