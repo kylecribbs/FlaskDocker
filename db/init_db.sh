63d3de08cb47bac6959c6589fdb7d2b9461fcb06
kylecribbs <31107878+kylecribbs@users.noreply.github.com>
2017-12-04 19:49:12 -0700

    fixed code

--- db/init_db.sh
+++ db/init_db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 /usr/bin/mysqld_safe --skip-grant-tables &
 sleep 5
-mysql -u root -e "CREATE DATABASE CLC"
-mysql -u root mydb < /tmp/dump.sql
+mysql -u root -e "CREATE DATABASE db625024825"
+mysql -u root db625024825 < /tmp/dump.sql
